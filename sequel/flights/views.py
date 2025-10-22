a31560ec2887bc5099429dd2c888dd03238edaef
Samaro <asamaroingwu@gmail.com>
2025-10-23 00:55:12 +0100

    created the individual flights page

--- sequel/flights/views.py
+++ sequel/flights/views.py
@@ -6,3 +6,9 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flights.objects.all()
     })
+
+def flight(request, flight_id):
+    flight = Flights.objects.get(pk= flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    }) 
