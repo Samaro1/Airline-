efe55457a6de10748d8003d0c67e3b505aec7a6a
Samaro <asamaroingwu@gmail.com>
2025-10-23 13:56:29 +0100

    Login view implemented

--- sequel/users/views.py
+++ sequel/users/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login,logout
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "user/login.html", {
+                "message": "Invalid credentials."
+            })
+    
+    return render(request, "user/login.html")
+
+def logout(request):
+    pass
